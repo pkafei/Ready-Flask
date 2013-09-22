3ce5410dc6f5537784a77dac6a21c8000b342d8a
Pkafei <plburton@gmail.com>
2013-09-22 12:44:46 -0700

    Added 404 page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 #-----------------------------
 # initialization
@@ -14,6 +14,16 @@ app.config.update(
 #------------------------------
 #controllers
 #------------------------------
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.htm'), 404
 
 
 @app.route("/")
