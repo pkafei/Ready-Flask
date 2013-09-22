5ddcacdf42aa308e8d32ca8a3dff67c241a3a5d3
Pkafei <plburton@gmail.com>
2013-09-22 12:32:10 -0700

    Changed base to an html file

--- app.py
+++ app.py
@@ -16,11 +16,6 @@ app.config.update(
 #------------------------------
 
 
-@app.errorhandler("404")
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
 @app.route("/")
 def index():
     return render_template('index.html')
