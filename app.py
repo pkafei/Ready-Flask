77361dcc32c4a53a0371d3115cce8bf402258259
Pkafei <plburton@gmail.com>
2013-09-25 16:04:49 -0700

    Added links to navbar

--- app.py
+++ app.py
@@ -26,14 +26,29 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@app.route("/")
-def index():
+@app.route('/')
+def base():
     return render_template('base.html')
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/tech')
+def tech():
+    return render_template('tech.html')
+
+
+@app.route('/graphs')
+def graphs():
+    return render_template('graphs.html')
 
 
 @app.route('/resources')
