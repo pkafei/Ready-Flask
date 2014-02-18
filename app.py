1cc846f70959ac264cf320b857e41a4cf5fce286
Portia Burton <plburton@gmail.com>
2014-02-17 22:12:33 -0800

    Rendering javascript

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template, send_from_directory
+from flask import url_for, redirect
+#from flask.ext.assets import Environment, Bundle
 
 #-----------------------------
 # initialization
@@ -12,14 +14,23 @@ app.config.update(
 )
 
 #------------------------------
-#controllers
+#Configuration
 #------------------------------
+
+#ASSETS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static/js')
+
+#app = Flask(__name__, template_folder=ASSETS_DIR, static_folder=ASSETS_DIR)
+
+
 
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
+@app.route('/')
+def javascript():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'js/hello.js')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -28,7 +39,7 @@ def page_not_found(e):
 
 @app.route('/')
 def base():
-    return render_template('index.html')
+    return render_template('test.html')
 
 
 #------------------------------
