81bd0266d0915d11bbe95d92fb4f00db4c2c7a05
Portia Burton <plburton@gmail.com>
2014-10-28 22:25:05 -0700

    Now renders bootstrap jumbo-narrow

--- app.py
+++ app.py
@@ -1,31 +1,24 @@
 import os
 from flask import Flask, render_template, send_from_directory
-from flask import url_for, redirect
-from flask_s3 import FlaskS3
-
-
-#from flask.ext.assets import Environment, Bundle
 
 #-----------------------------
 # initialization
 # -----------------------------
 
 app = Flask(__name__)
-app.config['jsflask'] = 'jsflask'
-s3 = FlaskS3(app)
-
 
 app.config.update(
     DEBUG=True,
 )
 
 #------------------------------
-#Configuration
+#controllers
 #------------------------------
 
-#ASSETS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static/js')
 
-#app = Flask(__name__, template_folder=ASSETS_DIR, static_folder=ASSETS_DIR)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
 
 @app.errorhandler(404)
@@ -35,7 +28,7 @@ def page_not_found(e):
 
 @app.route('/')
 def base():
-    return render_template('test.html')
+    return render_template('index.html')
 
 
 #------------------------------
